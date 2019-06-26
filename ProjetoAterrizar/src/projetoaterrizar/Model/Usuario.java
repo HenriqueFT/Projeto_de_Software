3b4823994dcf3b5b63421e7fbd70284b22beec87
HenriqueFT <henriqueft@id.uff.br>
2019-06-25 21:39:04 -0300

    Criado projeto NETBeans parao trabalho final. (Nao precisam continuar desenvolvendo no netbeans, soh tavaa  acostumado a usar ele pra java por isso iniciei nele)

---
+++ ProjetoAterrizar/src/projetoaterrizar/Model/Usuario.java
@@ -0,0 +1,65 @@
+package projetoaterrizar.Model;
+
+public class Usuario {
+    private int id;
+
+    
+    private String nome;
+    private String cpf;
+    private String email;
+    private String senha;
+
+    public Usuario(String nome, String cpf, String email, String senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+        //this.id = DB.generateID(); Metodo competado pelo banco de dados 
+    }
+    
+    
+    
+    public Cliente cadastrarCliente(String nome, String cpf, String email, String senha){
+        return null;
+    }
+    
+    public boolean validaSenha(String senha){
+        return false;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
